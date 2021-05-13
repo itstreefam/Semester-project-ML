fa52ea413a12f966062b358999b3a33ef6cd9044
Tri Pham <thientri399@gmail.com>
2021-05-13 05:26:58 -0500

    Update svm2.py, this model seems to work more accurately, though not as fast as sklearn's svm

--- svm2.py
+++ svm2.py
@@ -1,8 +1,8 @@
 import pickle
 import matplotlib.pyplot as plt
+import numba
 import numpy as np
 import pandas as pd
-import random as rn
 from sklearn.preprocessing import StandardScaler
 
 
@@ -18,35 +18,36 @@ class SVM:
             self.kernel = self.kernel_rbf
         else:
             print('Wrong kernel name')
+
         self.kernel_type = kernel_type
         self.regularization = regularization
         self.learning_rate = learning_rate
         self.tol = tol
-
         self.alphas = None
         self.w = None
         self.b = None
+        self.X = None
+        self.y = None
         np.random.seed(1234)
 
     def kernel_linear(self, x1, x2):
-        return x1 @ x2.T + self.b
+        return x1 @ x2.T
 
     def kernel_poly(self, x1, x2, degree=2):
         if degree == 1:
             self.kernel_linear(x1, x2)
         elif degree > 1:
-            return (x1 @ x2.T + self.b) ** degree
+            res = x1 @ x2.T
+            bias = 1
+            for i in range(len(res)):
+                res[i, :] += bias
+                res[i, :] = res[i, :] ** degree
+            return res
         else:
             print('Incorrect degree')
 
     def kernel_rbf(self, x1, x2, sigma=1):
-        if np.ndim(x1) == 1 and np.ndim(x2) == 1:
-            result = np.exp(- (np.linalg.norm(x1 - x2, 2)) ** 2 / (2 * sigma ** 2))
-        elif (np.ndim(x1) > 1 and np.ndim(x2) == 1) or (np.ndim(x1) == 1 and np.ndim(x2) > 1):
-            result = np.exp(- (np.linalg.norm(x1 - x2, 2, axis=1) ** 2) / (2 * sigma ** 2))
-        elif np.ndim(x1) > 1 and np.ndim(x2) > 1:
-            result = np.exp(-(np.linalg.norm(x1[:, np.newaxis] - x2[np.newaxis, :], 2, axis=2) ** 2) / (2 * sigma ** 2))
-        return result
+        return np.exp(- (np.linalg.norm(x1 - x2, 2, axis=-1) ** 2) / (2 * sigma ** 2)).reshape(-1, 1)
 
     def train(self, X, y):
         self.X = np.mat(SVM.normalize(X))
@@ -55,59 +56,60 @@ class SVM:
         self.b = 0.0
         self.alphas = np.mat(np.zeros((n, 1)))
         passes = 0
-        max_passes = 10
+        max_passes = int(self.max_iteration / 10)
 
         while passes < max_passes:
             num_changed_alphas = 0
             for i in range(n):
+                f_xi = float(np.multiply(self.alphas, self.y).T * self.kernel(self.X, self.X[i, :])) + self.b
+                E_i = f_xi - float(y[i])
 
-                fXi = float(np.multiply(self.alphas, self.y).T * (self.X * self.X[i, :].T)) + self.b
-                Ei = fXi - float(y[i])
-                if ((self.y[i] * Ei < -self.tol) and (self.alphas[i] < self.regularization)) or ((self.y[i] * Ei > self.tol) and (self.alphas[i] > 0)):
+                if (self.y[i] * E_i < -self.tol and self.alphas[i] < self.regularization) or (self.y[i] * E_i > self.tol and self.alphas[i] > 0):
 
                     j = self.get_rand_j(i, n)
 
-                    fXj = float(np.multiply(self.alphas, self.y).T * (self.X * self.X[j, :].T)) + self.b
-                    Ej = fXj - float(self.y[j])
+                    f_xj = float(np.multiply(self.alphas, self.y).T * self.kernel(self.X, self.X[j, :])) + self.b
+                    E_j = f_xj - float(self.y[j])
 
-                    alphaIold = self.alphas[i].copy()
-                    alphaJold = self.alphas[j].copy()
+                    prev_alpha_i = self.alphas[i].copy()
+                    prev_alpha_j = self.alphas[j].copy()
 
                     if self.y[i] != self.y[j]:
-                        L = max(0, self.alphas[j] - self.alphas[i])
-                        H = min(self.regularization, self.regularization + self.alphas[j] - self.alphas[i])
+                        (L, H) = max(0, self.alphas[j] - self.alphas[i]), min(self.regularization, self.regularization + self.alphas[j] - self.alphas[i])
                     else:
-                        L = max(0, self.alphas[j] + self.alphas[i] - self.regularization)
-                        H = min(self.regularization, self.alphas[j] + self.alphas[i])
+                        (L, H) = max(0, self.alphas[j] + self.alphas[i] - self.regularization), min(self.regularization, self.alphas[i] + self.alphas[j])
 
                     if L == H:
                         continue
 
-                    eta = 2.0 * self.X[i, :] * self.X[j, :].T - self.X[i, :] * self.X[i, :].T - self.X[j, :] * self.X[j, :].T
+                    eta = 2.0 * self.kernel(self.X[i, :], self.X[j, :]) - self.kernel(self.X[i, :], self.X[i, :]) - self.kernel(self.X[j, :], self.X[j, :])
 
                     if eta >= 0:
                         continue
 
-                    self.alphas[j] -= self.y[j] * (Ei - Ej) / eta
+                    self.alphas[j] = prev_alpha_j - self.y[j] * (E_i - E_j) / eta
 
-                    self.alphas[j] = self.clipAlphasJ(self.alphas[j], H, L)
+                    if self.alphas[j] > H:
+                        self.alphas[j] = H
+                    if self.alphas[j] < L:
+                        self.alphas[j] = L
 
-                    if abs(self.alphas[j] - alphaJold) < 0.00001:
+                    if abs(self.alphas[j] - prev_alpha_j) < 0.00001:
                         continue
 
-                    self.alphas[i] += self.y[j] * self.y[i] * (alphaJold - self.alphas[j])
+                    self.alphas[i] = prev_alpha_i + self.y[i] * self.y[j] * (prev_alpha_j - self.alphas[j])
 
-                    b1 = self.b - Ei - self.y[i] * (self.alphas[i] - alphaIold) * self.X[i, :] * self.X[i, :].T - self.y[j] * (self.alphas[j] - alphaJold) * self.X[i, :] * self.X[j, :].T
-                    b2 = self.b - Ej - self.y[i] * (self.alphas[i] - alphaIold) * self.X[i, :] * self.X[j, :].T - self.y[j] * (self.alphas[j] - alphaJold) * self.X[j, :] * self.X[j, :].T
+                    b1 = self.b - E_i - self.y[i] * (self.alphas[i] - prev_alpha_i) * self.kernel(self.X[i, :], self.X[i, :]) - self.y[j] * (self.alphas[j] - prev_alpha_j) * self.kernel(self.X[i, :], self.X[j, :])
+                    b2 = self.b - E_j - self.y[i] * (self.alphas[i] - prev_alpha_i) * self.kernel(self.X[i, :], self.X[j, :]) - self.y[j] * (self.alphas[j] - prev_alpha_j) * self.kernel(self.X[j, :], self.X[j, :])
 
-                    if (0 < self.alphas[i]) and (self.regularization > self.alphas[i]):
+                    if 0 < self.alphas[i] < self.regularization:
                         self.b = b1
-                    elif (0 < self.alphas[j]) and (self.regularization > self.alphas[j]):
+                    elif 0 < self.alphas[j] < self.regularization:
                         self.b = b2
                     else:
                         self.b = (b1 + b2) / 2.0
                     num_changed_alphas += 1
-                    # break
+                    break
                 if num_changed_alphas == 0:
                     passes += 1
                 else:
@@ -115,18 +117,8 @@ class SVM:
 
     def predict(self, X):
         X = SVM.normalize(X)
-        self.computeW()
+        self.compute_w()
         return np.sign(self.w.reshape(1, -1) @ X.T + self.b).reshape(-1, 1)
-        # pred = np.array([])
-        # for i in range(len(X)):
-        #     f = self.w.reshape(1, -1) @ X[i, :].reshape(-1, 1) + self.b
-        #     if f >= 0:
-        #         pred = np.append(pred, 1)
-        #     else:
-        #         pred = np.append(pred, -1)
-        # return pred
-        # return np.sign(self.w @ X.T + self.b)
-        # return self.f(X, self.w, self.b)
 
     def get_rand_j(self, i, m):
         j = i
@@ -134,14 +126,7 @@ class SVM:
             j = int(np.random.uniform(0, m))
         return j
 
-    def clipAlphasJ(self, aj, H, L):
-        if aj > H:
-            aj = H
-        if L > aj:
-            aj = L
-        return aj
-
-    def computeW(self):
+    def compute_w(self):
         self.w = np.zeros((self.X.shape[1], 1))
         for i in range(len(self.X)):
             self.w += np.multiply(self.alphas[i] * self.y[i], self.X[i, :].T)
